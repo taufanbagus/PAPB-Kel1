0c85f0e1c771f472f4bbca1d6a7e9e6206c5b5d5
luqmanrofifm <luqmanrmrofif@yahoo.co.id>
2021-03-21 17:22:22 +0700

    Add lifecycle onresume and ondestroy

--- app/src/main/java/com/example/biografi/Layout2.kt
+++ app/src/main/java/com/example/biografi/Layout2.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class Layout2 : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,4 +47,5 @@ class Layout2 : AppCompatActivity(), View.OnClickListener {
             }
         }
     }
+
 }
