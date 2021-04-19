9430cc32f8f5603a4837c737e96b50371c696031
luqmanrofifm <luqmanrmrofif@yahoo.co.id>
2021-04-19 12:06:18 +0700

    update db (not yet working)

--- E-linguistik/app/src/main/java/com/example/e_linguistik/adapter/HistoryAdapter.kt
+++ E-linguistik/app/src/main/java/com/example/e_linguistik/adapter/HistoryAdapter.kt
@@ -1,58 +1,73 @@
-package com.example.e_linguistik.adapter
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.e_linguistik
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-import android.widget.ListAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.e_linguistik.HistoryAdapter.HistoryViewHolder
 import com.example.e_linguistik.R
 import com.example.e_linguistik.db.HistoryModel
-import com.example.e_linguistik.adapter.HistoryAdapter.HistoryViewHolder
+import com.example.e_linguistik.db.HistoryRepository
 
-class HistoryAdapter: ListAdapter<HistoryModel, HistoryViewHolder>(HISTORY_COMPARATOR) {
+class HistoryAdapter : ListAdapter<HistoryModel, HistoryViewHolder>(HISTORY_COMPARATOR) {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
         return HistoryViewHolder.create(parent)
     }
 
     override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
         val current = getItem(position)
-        holder.bind(current.word)
+        holder.bind(current.typeTranslation, current.originWord, current.resultWordTranslation)
     }
 
     class HistoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val tvTypeHistory: TextView = itemView.findViewById(R.id.tv_history_type)
-        private val tvWordOrigin: TextView = itemView.findViewById(R.id.tv_word_origin)
-        private val tvWordTranslation : TextView = itemView.findViewById(R.id.tv_word_translation)
+        private val tvtypehsitory: TextView = itemView.findViewById(R.id.tv_history_type)
+        private val tvwordorigin: TextView = itemView.findViewById(R.id.tv_word_origin)
+        private val tvwordtranslation: TextView = itemView.findViewById(R.id.tv_word_translation)
 
-        fun bind(typeHistory: String?, wordOrigin: String?, wordTranslation: String? ){
-            tvTypeHistory.text = typeHistory
-            tvWordOrigin.text = wordOrigin
-            tvWordTranslation.text = wordTranslation
+        fun bind(type: String?, origin: String?, translation: String?) {
+            tvtypehsitory.text = type
+            tvwordorigin.text = origin
+            tvwordtranslation.text = translation
         }
 
-        companion object{
-            fun create(parent: ViewGroup): HistoryViewHolder{
+        companion object {
+            fun create(parent: ViewGroup): HistoryViewHolder {
                 val view: View = LayoutInflater.from(parent.context)
                         .inflate(R.layout.item_history, parent, false)
                 return HistoryViewHolder(view)
             }
         }
-
     }
 
-    companion object{
-        private  val HISTORY_COMPARATOR = object: DiffUtil.ItemCallback<HistoryModel>() {
+    companion object {
+        private val HISTORY_COMPARATOR = object : DiffUtil.ItemCallback<HistoryModel>() {
             override fun areItemsTheSame(oldItem: HistoryModel, newItem: HistoryModel): Boolean {
                 return oldItem === newItem
             }
 
             override fun areContentsTheSame(oldItem: HistoryModel, newItem: HistoryModel): Boolean {
-                TODO("Not yet implemented")
+                return oldItem.originWord == newItem.originWord
             }
-
         }
     }
 }
